b9342b86ee78c191fd15945ace1246314ad232ec
Guilherme Marques <guilhermecm01@gmail.com>
2021-11-01 18:14:29 -0300

    Biscoito da Sorte

--- App.js
+++ App.js
@@ -1,12 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View, Image, Text, TouchableOpacity } from "react-native";
 
 export default function App() {
+  const [img, setImg] = useState(require("./src/Image/biscoito.png"));
+  const [frase, setFrase] = useState("");
+
+  let frases = [
+    "A vida trará coisas boas se tiveres paciência.",
+    "Demonstre amor e alegria em todas as oportunidades e verás que a paz nasce dentro de você.",
+    "Não compense na ira o que lhe falta na razão",
+    "Não há que ser forte. Há que ser flexível.",
+    "Gente todo dia arruma os cabelos, por que não o coração.",
+    "A maior de todas as torres começa no solo.",
+  ];
+
+  function quebraBiscoito() {
+    let numeroAleatorio = Math.floor(Math.random() * frases.length);
+
+    setFrase(frases[numeroAleatorio]);
+    setImg(require("./src/Image/biscoitoAberto.png"));
+  }
+
+  function reiniciar() {
+    setImg(require("./src/Image/biscoito.png"));
+    setFrase("");
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={img} style={styles.img} />
+
+      <Text style={styles.textofrase}>{frase}</Text>
+
+      <TouchableOpacity style={styles.botao} onPress={quebraBiscoito}>
+        <View style={styles.btnArea}>
+          <Text style={styles.btnTexto}>Quebrar Biscoito</Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={reiniciar}
+        style={[styles.botao, { marginTop: 15, borderColor: "#121212" }]}
+      >
+        <View style={styles.btnArea}>
+          <Text style={[styles.btnTexto, { color: "#121212" }]}>
+            Reiniciar Biscoito
+          </Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +55,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  img: {
+    width: 230,
+    height: 230,
+  },
+  textofrase: {
+    fontSize: 20,
+    color: "#dd7b22",
+    margin: 30,
+    fontStyle: "italic",
+    textAlign: "center",
+  },
+  botao: {
+    width: 230,
+    height: 50,
+    borderColor: "#dd7b22",
+    borderWidth: 2,
+    borderRadius: 25,
+  },
+  btnArea: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  btnTexto: {
+    fontSize: 17,
+    fontWeight: "bold",
+    color: "#dd7b22",
   },
 });
